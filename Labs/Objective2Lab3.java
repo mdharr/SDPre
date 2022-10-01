ef9072ee142b3000ca625e49ad46437e00e279eb
Michael Harrington <mdharr7@gmail.com>
2022-10-01 17:45:52 +0800

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int birthYear = 1988;
+    String birthMonth = "November";
+    boolean isWeekend = false;
+
+    System.out.println(birthYear);
+    System.out.println(birthMonth);
+    System.out.println(isWeekend);
+  }
+}
