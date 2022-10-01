49c883819bc434688a6d0f5be71bd69cf8b80494
Michael Harrington <mdharr7@gmail.com>
2022-10-01 20:36:41 +0800

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,10 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int first, second, third, total;
+    first = 1 + (int)(Math.random() * 6);
+    second = 1 + (int)(Math.random() * 6);
+    third = 1 + (int)(Math.random() * 6);
+    total = first + second + third;
+    System.out.println(first + " + " + second + " + " + third + " = " + total);
+  }
+}
